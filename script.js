5bb98113bbec8166df38b7c9965fc314656113ca
Lokesh Nalamasa <107922763+lokeshgoud15@users.noreply.github.com>
2025-02-21 12:11:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,4 @@ $(document).ready(function () {
     $("#openModalBtn").click(function () {
         $("#signupModal").modal("show");
     });
-
-    $("#signupForm").submit(function (event) {
-        event.preventDefault();
-        var email = $("#emailInput").val();
-        var password = $("#passwordInput").val();
-        var confirmPassword = $("#confirmPasswordInput").val();
-
-        if (password !== confirmPassword) {
-            alert("Passwords do not match!");
-            return;
-        }
-
-        setTimeout(function () {
-            alert("Signup Successful!");
-            $("#signupModal").modal("hide");
-        }, 1000);
-    });
 });
