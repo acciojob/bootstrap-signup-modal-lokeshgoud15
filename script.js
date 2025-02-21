78e6f75984144ac7008e947004cfdfd6439ae465
Lokesh Nalamasa <107922763+lokeshgoud15@users.noreply.github.com>
2025-02-21 11:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-  document.addEventListener("DOMContentLoaded", function () {
+ document.addEventListener("DOMContentLoaded", function () {
         var myModal = new bootstrap.Modal(document.getElementById("signupModal"));
 
         document.getElementById("openModalBtn").addEventListener("click", function () {
-            myModal.show();
+            myModal.show();  // This will show the modal and manage its display correctly.
         });
     });
