3e3689f6303b220c3fe983cea86116b022470542
Lokesh Nalamasa <107922763+lokeshgoud15@users.noreply.github.com>
2025-02-21 11:36:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+  document.addEventListener("DOMContentLoaded", function () {
+        var myModal = new bootstrap.Modal(document.getElementById("signupModal"));
+
+        document.getElementById("openModalBtn").addEventListener("click", function () {
+            myModal.show();
+        });
+    });
